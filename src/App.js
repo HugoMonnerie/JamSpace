08cb55a71cff9426aa5f98180ea72d15019f8296
Hugo Monnerie <hugo.monnerie@edu.itescia.fr>
2022-06-30 09:44:12 +0200

    WIP: select departments

--- src/App.js
+++ src/App.js
@@ -1,24 +1,101 @@
-import logo from './logo.svg';
 import './App.css';
+
+let users = require('./component/data/data.json');
+
+const listUserName = users.map((user) =>
+    <li key={user.id}>{user.name}</li>
+);
+
+/*function searchUser(department) {
+    /*users.map((user) =>
+        user['departments'].forEach(departmentUser => {
+            if (departmentUser === department) {
+                console.log(user.name)
+            }
+        }),
+    );
+}*/
+
+var listDepartment = [];
+
+users.forEach(user =>
+    user['departments'].forEach(department => {
+        listDepartment.push(department)
+    })
+);
+
+const uniqueDepartments = [];
+
+listDepartment.forEach((department, id) => {
+    if (listDepartment.indexOf(department) === id){
+        uniqueDepartments.push(department);
+    }
+})
+
+console.log('uniqueDepartments', uniqueDepartments)
+
+let nameUniqueDepartment = [];
+
+uniqueDepartments.forEach((department) => {
+    const url = 'https://geo.api.gouv.fr/departements/' + department;
+    fetch(url)
+        .then(async response => {
+            const data = await response.json();
+
+            if (!response.ok) {
+                const error = (data && data.message) || response.statusText;
+                return Promise.reject(error);
+            }
+
+            //nameUniqueDepartment = new Map({'department': data['nom']});
+            nameUniqueDepartment.push(data['nom']);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+})
+
+console.log('nameUniqueDepartment', nameUniqueDepartment)
+
+/*const [value, setValue] = React.useState('rien');
+
+const handleChange = (event) => {
+    setValue(event.target.value);
+};*/
+
+
+
+/*const department = [];
+
+for (var i=0; i<nameUniqueDepartment.length; i++) {
+    console.log('bli');
+    department.push(nameUniqueDepartment[i]);
+}
+
+console.log('department', department)*/
+
+/*let nameDepartments = Object.keys(nameUniqueDepartment).map(nameDepartment => {
+    return (
+        <option value={nameDepartment}>{`${nameDepartment}: ${nameUniqueDepartment[nameDepartment]}`}</option>
+    );
+});*/
+function selector() {
+    for (let i = 0; i < 22; i++) {
+        <option value={nameUniqueDepartment[i]}>{nameUniqueDepartment[i]}</option>
+    }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <ul>{listUserName}</ul>
+          <label>
+              Filtrer par département :
+              <select>
+                  {selector}
+              </select>
+          </label>
+      </div>
   );
 }
 
